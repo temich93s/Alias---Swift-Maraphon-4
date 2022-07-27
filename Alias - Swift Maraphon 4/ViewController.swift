8ca6d16bd2128c1d7296e1f5bc703d361f5a176e
Temich93s <nil>
2022-07-27 13:20:24 +1000

    Initial Commit

---
+++ Alias - Swift Maraphon 4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Alias - Swift Maraphon 4
+//
+//  Created by 2lup on 27.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
